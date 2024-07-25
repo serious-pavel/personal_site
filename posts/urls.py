dd50afa9213250f8ae3099d7d5689b44f7a32e4f
serious-pavel <pvmakhnev@gmail.com>
2024-07-25 18:12:42 +0200

    remove old python posts

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.posts, name='posts'),
-    path('posts_old/<str:article_slug>', views.post_old, name='post_old'),
     path('posts/<str:slug>', views.post, name='post'),
 ]
