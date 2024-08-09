b82b7e938c66abdb4d47db261a8fcf59a9537e9c
serious-pavel <pvmakhnev@gmail.com>
2024-08-09 13:26:06 +0200

    remove redundant code, list only fields that user need to see

--- posts/forms.py
+++ posts/forms.py
@@ -3,22 +3,14 @@ from django import forms
 
 from .models import Comment
 
-# class ReviewForm(forms.Form):
-#     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
-#         "required": "Your name must not be empty!",
-#         "max_length": "Please enter a shorter name!"
-#     })
-#     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
-#     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
-
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = ["comment_author", "comment_content",]
         labels = {
             "comment_author": "Your Name",
-            "comment_content": "Your Your Comment",
+            "comment_content": "Your Comment",
         }
         error_messages = {
             "user_name": {
