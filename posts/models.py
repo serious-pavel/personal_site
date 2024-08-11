21e92e63305d4716e5e3bb427ac9759472e972c4
serious-pavel <pvmakhnev@gmail.com>
2024-08-11 21:55:22 +0200

    add date_created field

--- posts/models.py
+++ posts/models.py
@@ -43,6 +43,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    date_created = models.DateTimeField(auto_now_add=True)
     comment_author = models.CharField(max_length=50)
     comment_content = models.TextField()
 
