0a7dafbc42771137740ad34f36d8c44e29a04b51
serious-pavel <pvmakhnev@gmail.com>
2024-08-14 18:19:17 +0200

    remove redundant code (labels), add placeholders to inputs through widgets

--- posts/forms.py
+++ posts/forms.py
@@ -8,13 +8,19 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["comment_author", "comment_content",]
-        labels = {
-            "comment_author": "Your Name",
-            "comment_content": "Your Comment",
-        }
         error_messages = {
             "user_name": {
                 "required": "Your name must not be empty!",
                 "max_length": "Please enter a shorter name!"
             }
+        }
+        widgets = {
+            "comment_author": forms.TextInput(attrs={
+                # "class": "comment-author-input",
+                "placeholder": "Your Name",
+            }),
+            "comment_content": forms.Textarea(attrs={
+                # "class": "comment-content-input",
+                "placeholder": "Your Comment",
+            })
         }
